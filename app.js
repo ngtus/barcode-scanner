816f316817c6d2217b922ca38b30f0a3212d3577
ngtus <tunguyenminh065@gmail.com>
2026-02-20 19:09:48 +0700

    add beep, vibrate, local sheet

--- app.js
+++ app.js
@@ -1,3 +1,72 @@
+// Beep on scanned
+const beep = new Audio("https://actions.google.com/sounds/v1/alarms/beep_short.ogg");
+
+function feedback() {
+  beep.play();
+
+  if (navigator.vibrate) {
+    navigator.vibrate(100);
+  }
+}
+
+// Local storage
+let inventory = JSON.parse(localStorage.getItem("inventory")) || {};
+
+function saveItem(code) {
+  if (!inventory[code]) {
+    inventory[code] = {
+      count: 0,
+      lastSeen: new Date().toISOString()
+    };
+  }
+
+  inventory[code].count += 1;
+  inventory[code].lastSeen = new Date().toISOString();
+
+  localStorage.setItem("inventory", JSON.stringify(inventory));
+
+  renderInventory();
+}
+
+// Render inventory
+function renderInventory() {
+  const container = document.getElementById("inventory");
+  container.innerHTML = "";
+
+  Object.entries(inventory).forEach(([code, data]) => {
+    const div = document.createElement("div");
+    div.className = "item";
+    div.innerText = `${code} | count: ${data.count}`;
+    container.appendChild(div);
+  });
+}
+
+// Flash button
+let track;
+let flashOn = false;
+
+function enableFlash() {
+  const stream = Quagga.CameraAccess.getActiveStream();
+  if (!stream) return;
+
+  track = stream.getVideoTracks()[0];
+
+  const capabilities = track.getCapabilities();
+
+  if (!capabilities.torch) {
+    alert("Flash not supported 😢");
+    return;
+  }
+
+  flashOn = !flashOn;
+
+  track.applyConstraints({
+    advanced: [{ torch: flashOn }]
+  });
+}
+
+document.getElementById("flashBtn").onclick = enableFlash;
+
 Quagga.init({
   inputStream: {
     type: "LiveStream",
@@ -23,5 +92,11 @@ Quagga.init({
 
 Quagga.onDetected(function(data) {
   const code = data.codeResult.code;
-  document.getElementById("result").innerText = "Result: " + code;
+
+  resultEl.innerText = "Scanned: " + code;
+  renderInventory();
+
+  feedback();
+  saveItem(code);
 });
+
